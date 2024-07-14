528d7d3d4f85926beef500f89c9a6bbc11e50090
kihoo-ni <gudcjf532@gmail.com>
2024-07-14 15:54:10 +0900

    feature "rabbitmq 설정"

---
+++ src/main/java/com/nhnacademy/bookstorecoupon/global/config/RabbitMQConfig.java
@@ -0,0 +1,41 @@
+package com.nhnacademy.bookstorecoupon.global.config;
+
+import java.util.Queue;
+
+import org.apache.commons.dbcp2.ConnectionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableRabbit
+public class RabbitMQConfig {
+
+	public static final String COUPON_ISSUE_QUEUE = "couponIssueQueue";
+
+	@Bean
+	public Queue couponIssueQueue() {
+		return new Queue(COUPON_ISSUE_QUEUE, false);
+	}
+
+	@Bean
+	public Jackson2JsonMessageConverter jsonMessageConverter() {
+		return new Jackson2JsonMessageConverter();
+	}
+
+	@Bean
+	public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+		RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+		rabbitTemplate.setMessageConverter(jsonMessageConverter());
+		return rabbitTemplate;
+	}
+
+	@Bean
+	public SimpleMessageListenerContainer messageListenerContainer(ConnectionFactory connectionFactory,
+		CouponIssuanceListener couponIssuanceListener) {
+		SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+		container.setConnectionFactory(connectionFactory);
+		container.setQueues(couponIssueQueue());
+		container.setMessageListener(couponIssuanceListener);
+		return container;
+	}
+}
