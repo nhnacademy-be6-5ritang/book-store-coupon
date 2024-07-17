cc2117ea975919114ee30c66505b9774d5e89973
kihoo-ni <gudcjf532@gmail.com>
2024-07-17 18:11:08 +0900

    feature "validation"

--- src/main/java/com/nhnacademy/bookstorecoupon/couponpolicy/domain/dto/request/CouponPolicyUpdateRequestDTO.java
+++ src/main/java/com/nhnacademy/bookstorecoupon/couponpolicy/domain/dto/request/CouponPolicyUpdateRequestDTO.java
@@ -3,8 +3,7 @@ package com.nhnacademy.bookstorecoupon.couponpolicy.domain.dto.request;
 import java.math.BigDecimal;
 
 import jakarta.validation.constraints.NotNull;
-import lombok.NonNull;
 
 public record CouponPolicyUpdateRequestDTO(@NotNull BigDecimal minOrderPrice, BigDecimal salePrice, BigDecimal saleRate,
-										   BigDecimal maxSalePrice, @NonNull Boolean isUsed) {
+										   BigDecimal maxSalePrice, @NotNull Boolean isUsed) {
 }
