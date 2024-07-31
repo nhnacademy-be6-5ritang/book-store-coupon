cb2eb97b08366b83943d80618d1ebb3940a71790
kihoo-ni <gudcjf532@gmail.com>
2024-07-31 15:56:21 +0900

    javadoc "쿠폰 javadoc 설정"

--- src/main/java/com/nhnacademy/bookstorecoupon/bookcoupon/repository/CustomBookCouponRepository.java
+++ src/main/java/com/nhnacademy/bookstorecoupon/bookcoupon/repository/CustomBookCouponRepository.java
@@ -4,6 +4,18 @@ import java.util.Map;
 
 import com.nhnacademy.bookstorecoupon.bookcoupon.domain.entity.BookCoupon;
 
+
+/**
+ * @author 이기훈
+ * 도서 쿠폰 테이블에 있는 정보를 querydsl을 통해 가져오는 custom repository
+ */
+
 public interface CustomBookCouponRepository {
+
+
+	/**
+	 * 도서 쿠폰 테이블에 있는 정보를 querydsl을 통해 가져오는 custom repository
+	 * @return 쿠폰정책 아이디, 북 아이디, 도서제목
+	 */
 	Map<Long, BookCoupon.BookInfo> fetchBookIdMap();
 }
