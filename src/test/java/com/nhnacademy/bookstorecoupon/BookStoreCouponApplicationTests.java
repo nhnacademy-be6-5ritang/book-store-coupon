56a23e084ab19620c29109fde6b08e9fba6fcb34
dlrudgjs104 <dlrudgjs104@naver.com>
2024-08-02 02:38:56 +0900

    test: 테스트 코드 수정

--- src/test/java/com/nhnacademy/bookstorecoupon/BookStoreCouponApplicationTests.java
+++ src/test/java/com/nhnacademy/bookstorecoupon/BookStoreCouponApplicationTests.java
@@ -3,7 +3,6 @@ package com.nhnacademy.bookstorecoupon;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class BookStoreCouponApplicationTests {
 
     @Test
